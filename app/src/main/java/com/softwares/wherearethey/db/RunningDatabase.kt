e7dacae8dff9f68ccbb4ae8e5fd388cb12b42bdd
tazeking <softwares4world@gmail.com>
2021-11-20 01:36:53 +0530

    Added Type Convertor for Bitmap and Database Class for Room

---
+++ app/src/main/java/com/softwares/wherearethey/db/RunningDatabase.kt
@@ -0,0 +1,12 @@
+package com.softwares.wherearethey.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+
+@Database(entities = [Run::class],version = 1)
+@TypeConverters(Converters::class)
+abstract class RunningDatabase : RoomDatabase() {
+    abstract fun getRunDao() : RunDao
+}
